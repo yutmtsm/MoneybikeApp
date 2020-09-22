25e344887bccd56ce5f1db3aa31fa9dfbbb98f19
”yutmtsm” <yutmtsm@gmail.com>
2020-09-22 14:02:59 +0000

    original App

--- app/Http/Controllers/Admin/MoneybikeController.php
+++ app/Http/Controllers/Admin/MoneybikeController.php
@@ -93,26 +93,26 @@ class MoneybikeController extends Controller
         //検索⇨投稿記事
         if($cond_title != ''){
             // 検索されたら検索結果を取得する
-            $posts = DB::table('tweets')->where('title', 'like', "%$cond_title%")->orwhere('comment', 'like', "%$cond_title%")->orwhere('created_at', 'like', "%$cond_title%")->orwhere('spot', 'like', "%$cond_title%")->orwhere('pref', 'like', "%$cond_title%")->orderByDesc('created_at')->simplePaginate(4);
+            $timelines = DB::table('tweets')->where('title', 'like', "%$cond_title%")->orwhere('comment', 'like', "%$cond_title%")->orwhere('created_at', 'like', "%$cond_title%")->orwhere('spot', 'like', "%$cond_title%")->orwhere('pref', 'like', "%$cond_title%")->orderByDesc('created_at')->simplePaginate(4);
         } else {
-            $posts = DB::table('tweets')->orderByDesc('created_at')->simplePaginate(3);
+            $timelines = DB::table('tweets')->orderByDesc('created_at')->simplePaginate(3);
         }
         
-        foreach($posts as $post){
-            $users = User::find($post->user_id);
-            $post->user_name = $users->name;
-            $post->screen_name = $users->screen_name;
+        foreach($timelines as $timeline){
+            $users = User::find($timeline->user_id);
+            $timeline->user_name = $users->name;
+            $timeline->screen_name = $users->screen_name;
             if($users->image_path != null){
-                $post->image_icon = $users->image_path;
+                $timeline->image_icon = $users->image_path;
                 // dd($post->image_icon);
             } else {
-                $post->image_icon = null;
+                $timeline->image_icon = null;
             }
             
         }
         
         return view('admin.spot_search', [
-        'user' => $user, 'all_users' => $all_users, 'posts' => $posts,
+        'user' => $user, 'all_users' => $all_users, 'timelines' => $timelines,
         'cond_title' => $cond_title,
         ]);
     }
--- resources/views/admin/spot_search.blade.php
+++ resources/views/admin/spot_search.blade.php
@@ -39,53 +39,66 @@
                                 
                             </div>
                         </div>
-                        @foreach($posts as $post)
-                        
-                            <div class="post">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        
-                                         <div class="title">
-                                             <h2>{{ str_limit($post->title, 100) }}</h2>
+                        @foreach ($timelines as $timeline)
+                        <a href="{{ action('Admin\TweetsController@show', ['id' => $timeline->id]) }}">
+                            <div class="col-md-8 mb-3">
+                                <div class="card">
+                                    <div class="card-haeder p-2 w-100 d-flex">
+                                        <img src="{{ asset('storage/profile_image/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50">
+                                        <div class="ml-2 d-flex flex-column">
+                                            <p class="mb-0">{{ $timeline->user->name }}</p>
+                                            <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">{{ $timeline->user->screen_name }}</a>
                                         </div>
-                                        <div class="post-info d-flex">
-                                            <div class="col-md-8 d-flex no-gutters">
-                                                <div class="d-flex">
-                                                    <img src="{{ asset('storage/profile_image/' .$post->image_icon) }}" class="rounded-circle" width="50" height="50">
-                                                    <div class="ml-2 d-flex flex-column">
-                                                        <p class="mb-0">{{ $post->user_name }}</p>
-                                                        <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $post->screen_name }}</a>
-                                                    </div>
-                                                </div>
-                                                <div class="post-spot">　エリア：{{ $post->spot }}</div>
-                                            </div>
+                                        <div class="m-10">
+                                            <p class="mb-0 text-secondary text-right">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
+                                            <h3>『{{ $timeline->title }}』</h3>
                                         </div>
-                                        <div class="d-flex">
-                                            <div class="spot-image">
-                                                @if(isset($post->image_path))
-                                                    <img src="/storage/image/post/{{ $post->image_path }}">
-                                                @else
-                                                    <img src="/storage/image/noimage.png">
-                                                @endif
-                                            </div>
-                                            <div class="post-text">
-                                                <div class="spot-text mt-3">
-                                                    <p>{{ str_limit($post->text, 1500) }}</p>
-                                                </div>
-                                                <div class="detail-btn">
-                                                    <a href="{{ action('Admin\TweetsController@show', ['id' => $post->id]) }}">
-                                                        <button type="button" class="btn btn-primary btn-xs active" data_but="btn-xs"><i class='fa fa-search'></i> 詳しく見る</button>
-                                                    </a>
+                                    </div>
+                                    <div class="card-body">
+                                        {{ $timeline->text }}
+                                    </div>
+                                    <div class="card-footer py-1 d-flex justify-content-end bg-white">
+                                        @if ($timeline->user->id === Auth::user()->id)
+                                            <div class="dropdown mr-3 d-flex align-items-center">
+                                                <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v fa-fw"></i>
+                                                </a>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                    <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
+                                                    <a href="{{ action('Admin\TweetsController@delete', ['id' => $timeline->id]) }}" class="dropdown-item">削除</a>
                                                 </div>
                                             </div>
+                                        @endif
+            
+                                        <!-- ここから -->
+                                        <div class="mr-3 d-flex align-items-center">
+                                            @include('layouts.common.comments_modal')
+                                            <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
                                         </div>
-                                        <div class="date text-right">
-                                            {{ $post->created_at }}
+                                        <div class="d-flex align-items-center">
+                                            @if (!in_array(Auth::user()->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
+                                                <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
+                                                    @csrf
+            
+                                                    <input type="hidden" name="tweet_id" value="{{ $timeline->id }}">
+                                                    <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
+                                                </form>
+                                            @else
+                                                <form method="POST"action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[Auth::user()->id]) }}" class="mb-0">
+                                                    @csrf
+                                                    @method('DELETE')
+            
+                                                    <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
+                                                </form>
+                                            @endif
+                                            <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
                                         </div>
+                                        <!-- ここまで -->
+            
                                     </div>
                                 </div>
                             </div>
-                        
+                        </a>
                         @endforeach
                         {{ $posts->links() }}
                     </div>
