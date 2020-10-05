6ec5fc99533544183a289fda234eeb5f7823ee0a
”yutmtsm” <yutmtsm@gmail.com>
2020-10-05 02:53:37 +0000

    スライダー実装（bootstrap）

--- resources/views/nologin/top.blade.php
+++ resources/views/nologin/top.blade.php
@@ -21,28 +21,44 @@
         </div><!--top-comment-->
     </div>
 </div><!--top-wrapper-->
-<div id="service-wrapper" class="mt-5">
-    <div class="container">
-        <a href="#timeline"><div  class="mainVisual"></div></a>
-        <h2 class="heading text-center">バイクのお金周りに関するアプリ</h2>
-    	<div class="projects row pt-4">
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top1.jpg') }}"><p>ツーリングスポットの検索＆使途金</p></div>
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top2.jpg') }}"><p>他ユーザーの維持費情報</p></div>
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top3.jpg') }}"><p>マイバイクのお金管理</p></div>
+
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-interval=3000 data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div id="service-wrapper" class="mt-5">
+                    <a href="#timeline"><div  class="mainVisual"></div></a>
+                    <h2 class="heading text-center">バイクのお金周りに関するアプリ</h2>
+                    <div class="projects row pt-4">
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top1.jpg') }}"><p>ツーリングスポットの検索</p></div>
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top2.jpg') }}"><p>スポットでのお金</p></div>
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top3.jpg') }}"><p>ユーザーの維持費情報</p></div>
+                    </div>
+                </div><!--service-wrapper-->
+            </div>
+            <div class="carousel-item">
+                <div id="service-wrapper" class="mt-5">
+                    <a href="#timeline"><div  class="mainVisual"></div></a>
+                    <h2 class="heading text-center">ログインして記事の投稿やバイク関連のお金管理</h2>
+                	<div class="projects row pt-4">
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top1.jpg') }}"><p>行った場所・使ったお金を投稿</p></div>
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top2.jpg') }}"><p>使ったお金が一目でわかる</p></div>
+                    	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top3.jpg') }}"><p></p></div>
+                    </div>
+                </div><!--service-wrapper-->
+            </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-</div><!--ervice-wrapper-->
-<div id="about-wrapper" class="mt-5">
-    <div class="container">
-        <a href="#timeline"><div  class="mainVisual"></div></a>
-        <h2 class="heading text-center">ログインして記事の投稿やバイク関連のお金管理</h2>
-    	<div class="projects row pt-4">
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top1.jpg') }}"><p>行った場所・使ったお金を投稿</p></div>
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top2.jpg') }}"><p>使ったお金が一目でわかる</p></div>
-        	<div class="projectsItem col-md-4"><img src="{{ asset('/images/top3.jpg') }}"><p></p></div>
-        </div>
-    </div>
-</div><!--about-wrapper-->
+</div>
+
 <div class="signup-wrapper">
     <div class="container">
         <div class="heading">
@@ -53,38 +69,4 @@
     </div>
 </div><!--message-wrapper-->
 
-<!-- Slider main container -->
-<div class="swiper-container">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        ...
-    </div>
-    <!-- If we need pagination -->
-    <div class="swiper-pagination"></div>
-    
-    <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-</div>
-<script type="text/javascript" src="">
-    var mySwiper = new Swiper ('.swiper-container', {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 10,
-  centeredSlides : true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  breakpoints: {
-    767: {
-      slidesPerView: 1,
-      spaceBetween: 0
-    }
-  }
-})
-</script>
 @endsection
