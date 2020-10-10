be8ecfa9cfeb2d032a432d06028fad650df6235f
”yutmtsm” <yutmtsm@gmail.com>
2020-10-10 23:28:57 +0000

    ログインモーダル　お試し

---
+++ public/js/modal.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+});
--- resources/views/layouts/common/common.blade.php
+++ resources/views/layouts/common/common.blade.php
@@ -23,20 +23,20 @@
         <!-- asset(‘ファイルパス’)は、publicディレクトリのパスを返す関数 -->
         <!-- Scripts -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>-->
         <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>-->
         <!--下のapp.jsで読み込んでいる為上３つはいらない-->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src='https://api.tiles.mapbox.com/mapbox.js/v2.0.0/mapbox.js'></script>
         <link href='https://api.tiles.mapbox.com/mapbox.js/v2.0.0/mapbox.css' rel='stylesheet' />
-        <script src="{{ asset('js/ktsn.lf.canvas.js') }}" defer></script>
+        <!--<script src="{{ asset('js/ktsn.lf.canvas.js') }}" defer></script>-->
          {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
         <script src="{{ asset('js/app.js') }}" defer></script>
         <!---->
         
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/infinite-scroll.pkgd.min.js') }}"></script>
+        <!--<script src="{{ asset('js/jquery.min.js') }}"></script>-->
+        <!--<script src="{{ asset('js/infinite-scroll.pkgd.min.js') }}"></script>-->
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -109,7 +109,8 @@
                         </li>
                         @endif
                         <li class="nav-item bg-dark">
-                            <a class="nav-link nav-link text-light" href="{{ route('login') }}">{{ __('messages.nav_login') }}</a>
+                            <a class="nav-link nav-link text-light" id="login-show">{{ __('messages.nav_login') }}</div>
+                            <!--<a class="nav-link nav-link text-light" href="{{ route('login') }}">{{ __('messages.nav_login') }}</a>-->
                         </li>
                         @endauth
                     </ul>
@@ -128,5 +129,84 @@
                 @endauth
             </footer>
         </div>
+        <div class="login" id="login-show">ログイン</div>
+        
+        <div class="login-modal-wrapper" id="login-modal">
+            <div class="modal">
+                <div class="close-modal">
+                    <i class="fa fa-2x fa-times"></i>
+                </div>
+                <!--ここから-->
+                <div class="row justify-content-center" style="margin-top: 30px;">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header" style="color: black;">{{ __('messages.Login') }}</div>
+            
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+            
+                                    <div class="form-group row">
+                                        <label for="email" class="col-md-4 col-form-label text-md-right" style="color: black;">{{ __('E-Mail Address') }}</label>
+            
+                                        <div class="col-md-6">
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+            
+                                    <div class="form-group row">
+                                        <label for="password" class="col-md-4 col-form-label text-md-right" style="color: black;">{{ __('Password') }}</label>
+            
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+            
+                                    <div class="form-group row">
+                                        <div class="col-md-6 offset-md-4">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            
+                                                <label class="form-check-label" for="remember" style="color: black;">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+            
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-8 offset-md-4">
+                                            <button type="submit" class="btn btn-primary" style="color: black;">
+                                                {{ __('Login') }}
+                                            </button>
+            
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--ここまで-->
+            </div>
+          </div>
+        <script src="{{ asset('js/modal.js') }}"></script>
     </body>
 </html>
